681c907d8915022782d27100955fe7603ce61a92
Jay Shollenberger <jay@jayshollenberger.com>
2022-07-05 14:17:20 -0600

    Enums

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import uvicorn
+from enum import Enum
 from fastapi import FastAPI
+
+
+class PersonName(str, Enum):
+    alex = "alex"
+    bob = "bob"
+
 
 app = FastAPI()
 
@@ -13,9 +20,19 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 @app.get("/users")
 async def read_users():
-    return ["Rick","Morty"]
+    return ["Rick", "Morty"]
+
+
+@app.get("/people/{person_name}")
+async def get_person(person_name: PersonName):
+    if person_name == PersonName.alex:
+        return {"description", "Its alex"}
+    if person_name == PersonName.bob:
+        return {"description", "Its BOB"}
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
